e2af57e00d7aa44ff49896e02191d1504af8f5ad
taolei <taolei615@qq.com>
2024-06-22 15:02:09 +0800

    1

--- js/timing.js
+++ js/timing.js
@@ -1,3 +1,7 @@
+function padNumber(number) {
+    return String(number).padStart(2, '0');
+}
+
 function updateTimer1() {
     const startDate = new Date('2024-03-29T00:00:00'); // 修改为你们在一起的起始日期
     const now = new Date();
@@ -6,8 +10,9 @@ function updateTimer1() {
     const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
     const minutes = Math.floor((diff / 1000 / 60) % 60);
     const seconds = Math.floor((diff / 1000) % 60);
-    document.getElementById('time1').innerText = `${days}天${hours}小时${minutes}分${seconds}秒`;
+    document.getElementById('time1').innerText = `${padNumber(days)}天${padNumber(hours)}小时${padNumber(minutes)}分${padNumber(seconds)}秒`;
 }
+
 function updateTimer2() {
     const startDate = new Date('2021-11-19T00:00:00'); // 修改为你们认识的起始日期
     const now = new Date();
@@ -16,8 +21,9 @@ function updateTimer2() {
     const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
     const minutes = Math.floor((diff / 1000 / 60) % 60);
     const seconds = Math.floor((diff / 1000) % 60);
-    document.getElementById('time2').innerText = `${days}天${hours}小时${minutes}分${seconds}秒`;
+    document.getElementById('time2').innerText = `${padNumber(days)}天${padNumber(hours)}小时${padNumber(minutes)}分${padNumber(seconds)}秒`;
 }
+
 setInterval(updateTimer1, 1000);
 setInterval(updateTimer2, 1000);
 updateTimer1(); // 初始化调用
